6a7b63ab059f1b8ff037ca66b5c103c2a89b9f52
pedro do 7 <pedrohbarbosa0929@gmail.com>
2026-05-12 12:07:59 -0300

    Add files via upload

---
+++ aula08/app.py
@@ -0,0 +1,23 @@
+#def funcao_segundo_grau(a, b, c):
+   # print("Olá, mundo!")
+  #  return a, b, c
+
+# chamando a funcao e armazenando o valor em uma variavel
+#x = funcao_segundo_grau(1, 2, 3)
+#print(x)
+
+#def soma(a,b):
+   # resultado = a + b
+    #return resultado
+
+#num1 = int(input("Digite um numero qualquer: "))
+#num2 = int(input("Digite um numero qualquer: "))
+
+#resultado = soma(num1,num2)
+#print(resultado)
+
+def mostrar_saudacao(nome):
+    print(f'Olá {nome}, seja bem vindo!')
+
+mostrar_saudacao('barbosa')
+
